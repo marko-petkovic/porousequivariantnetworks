b6fcfa670cd65129df790bf7e8f0228f39de4949
merkelmauer <markopetkovic1999@gmail.com>
2023-10-10 00:59:19 +0200

    Update train.py

--- code/train/train.py
+++ code/train/train.py
@@ -93,7 +93,7 @@ if __name__ == "__main__":
                             idx1_sp.to('cuda'), idx2_sp.to('cuda'), idx2_oh_sp.to('cuda'), 
                             idx1_ps.to('cuda'), idx2_ps.to('cuda'), idx2_oh_ps.to('cuda'),
                             hid_size=[8]*6, site_emb_size=8, edge_emb_size=8, mlp_size=24,
-                            centers=10, mx_d=6, width=1, pool='sum', pool_pore=args.aggregate_pore).to('cuda')
+                            centers=10, mx_d=6, width=1, pool='sum', pool_pore=args.aggregate_pore, set=True).to('cuda')
             _, testloader, trainloader = get_data_pore(atoms, hoa, edges, pore, edges_sp, edges_ps, bs=32, sub_lim=args.sub_lim, p=args.prop_train, random=args.random_split)
 
         
